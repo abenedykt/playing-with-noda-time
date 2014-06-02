022d8243af15e2d9012c6a87ffabf3fc826ca536
abenedykt <arkadiusz@benedykt.net>
2014-06-02 21:08:59 +0200

    initial commit

---
+++ NodaTimeTestings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NodaTime;
+
+namespace NodaTimeTestings
+{
+	class Program
+	{
+		static void Main()
+		{
+			Instant now = SystemClock.Instance.Now;
+			Console.WriteLine("now       : {0}",now);
+			ZonedDateTime utcNow = now.InUtc();
+			Console.WriteLine("utcNow    : {0}",utcNow);
+			var poland = DateTimeZoneProviders.Tzdb["Europe/Warsaw"];
+
+			var polandTime = now.InZone(poland);
+			Console.WriteLine("in poland : {0}", polandTime);
+		}
+	}
+}
